98be9a83fcbcb3df6e56b3f7d74d2bcfba41a1af
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-01-30 22:05:22 +0900

    그리디 예제 4 1이 될때 까지
    2023-01-30 오후 10:04

---
+++ book/greedy/example_4_solution.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+result = 0
+
+while N > K:
+    while N % K != 0:
+        N -= 1
+        result += 1
+    N //= K
+    result += 1
+
+while N > 1:
+    N -= 1
+    result += 1
+
+print(result)
